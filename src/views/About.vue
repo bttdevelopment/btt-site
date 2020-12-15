f79a8eac13126c2b414b76b02035d1c951b1d2a6
javkhlantugs <jay.mudgil@gmail.com>
2020-12-15 15:15:59 -0500

    testing video in about to test Netlify large Media

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,8 @@
 <template>
-    <div class="about" style="height:500px"></div>
+    <div class="about" style="height:500px">
+        <video width="320" height="240" controls>
+            <source src="../assets/brainloop1.mp4" type="video/mp4">
+Your browser does not support the video tag.
+</video>
+    </div>
 </template>
